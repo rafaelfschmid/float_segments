9ef8e1dfb6c92e22edafdfcfb24df5f398591da7
schmid <Rafael Schmid>
2016-05-31 09:00:07 -0300

    first commit

---
+++ src/scripts/parsetimes.sh
@@ -0,0 +1,12 @@
+dir=$1
+
+./parser.exe $dir/fixbitonic.time $dir/00fixbitonic.time
+./parser.exe $dir/fixpreprocess.time $dir/00fixpreprocess.time
+./parser.exe $dir/fixcub.time $dir/00fixcub.time
+./parser.exe $dir/fixmerge.time $dir/00fixmerge.time
+./parser.exe $dir/fixoddeven.time $dir/00fixoddeven.time
+./parser.exe $dir/fixquick.time $dir/00fixquick.time
+./parser.exe $dir/fixthrust.time $dir/00fixthrust.time
+./parser.exe $dir/mergeseg.time $dir/00mergeseg.time
+./parser.exe $dir/radixseg.time $dir/00radixseg.time
+
